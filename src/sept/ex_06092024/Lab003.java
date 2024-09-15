b2dcf3d6a09b82134e9d5358c4e979f4171924a5
Kavita <banekavita26@gmail.com>
2024-09-16 00:07:23 +0530

    Add the files

---
+++ src/sept/ex_06092024/Lab003.java
@@ -0,0 +1,14 @@
+package ex_06092024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+2);
+    }
+    public static void main(String args) {
+        System.out.println(6-1);
+    }
+    public void main(int args){
+        System.out.println(5*2);
+    }
+}
---
+++ src/sept/ex_06092024/Lab004.java
@@ -0,0 +1,12 @@
+package ex_06092024;
+/* Multiline comment
+ABC
+ */
+public class Lab004 {
+    public static void main(String[] args) {
+        //Single line comment
+        //This will print "Hello world
+        System.out.println("Hello World");
+
+    }
+}
